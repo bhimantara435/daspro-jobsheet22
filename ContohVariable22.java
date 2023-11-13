6052e07bcf752cb192234c8cd031b754dae7e2d2
bhimantara435 <144045671+bhimantara435@users.noreply.github.com>
2023-11-13 20:26:34 +0700

    Add files via upload

---
+++ ContohVariable22.java
@@ -0,0 +1,18 @@
+public class ContohVariable22 {
+    public static void main(Strings[] args) {
+        Strings salahSatuHobySayaAdalah = "Bermain petak umpet" ;
+        boolen isPandai = true;
+        char jenisKelamin = 'L' ;
+        byte _umurSayaSekarang = 20 ;
+        double $ipk = 3.24, tinggi = 1.78;
+        System.out.println(salahSatuHobySayaAdalah) ;
+        System.out.println("Apakah pandai? " + isPandai) ;
+        System.out.println("Jenis kelamin: " + JenisKelamin) ;
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang) ;
+        System.out.println("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi) ;
+        
+
+
+
+    }
+}
