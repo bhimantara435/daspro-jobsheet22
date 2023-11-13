61e866cbfa12baa522e8a84cdffca62f55a8717d
bhimantara435 <144045671+bhimantara435@users.noreply.github.com>
2023-11-13 20:21:52 +0700

    Add files via upload

---
+++ Jobsheet 11/Square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * Square
+ */
+public class Square {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai N = ");
+        int N = sc.nextInt();
+
+        for 
+        for(int i=1 i<=N; i++){
+            System.out.print("*");
+        }
+    }
+}
